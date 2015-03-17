c1a556ae13d102d55661787eadf63aa16e26b679
tmdavid <torrejonmoya.david@gmail.com>
2015-03-17 22:27:03 +0100

    Tested first stupid class definitions, call to functions and stuff

--- NPLmain.py
+++ NPLmain.py
@@ -1,7 +1,13 @@
 __author__ = 'User'
 
+import dataExtractor as dE
+
 # @tmdavid
 # NPL first steps???
 #
 
+dataE = dE.dataExtraction()
+
 print("there we go")
+
+dataE.dataFrom(2)
--- dataExtractor.py
+++ dataExtractor.py
@@ -1,1 +1,19 @@
 __author__ = 'User'
+
+
+def twitter():
+        print("info from twitter")
+
+
+def facebook():
+        print("info from facebook")
+
+class dataExtraction():
+
+    def __init__(self):
+        return
+
+    def dataFrom(self, where):
+        switchDataFrom = {1: twitter, 2: facebook}
+        switchDataFrom[where]()
+        return
